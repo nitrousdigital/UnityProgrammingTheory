bbe1a346dc9d2b66d9426097892bb399a435a4ec
nitrousdigital <nitrousdigital@gmail.com>
2022-04-19 20:04:40 -0400

    title screen animation

--- Assets/Scripts/TitleScene/TitleSceneController.cs
+++ Assets/Scripts/TitleScene/TitleSceneController.cs
@@ -15,27 +15,44 @@ public class TitleSceneController : MonoBehaviour
     /// </summary>
     [SerializeField] private float playerCycleSpeed = 0.5f;
 
+    /// <summary>
+    ///  The size of the sin wave (distance in pixels from the horizontal origin)
+    /// </summary>
+    [SerializeField] private float enemySinWaveMagnitude = 0.3f;
+
+    /// <summary>
+    ///  The speed at which the ship will move along the sine wave
+    /// </summary>
+    [SerializeField] private float enemyCycleSpeed = 0.5f;
 
     [SerializeField] private GameObject enemyTorpedoPrefab;
     [SerializeField] private GameObject enemy;
     [SerializeField] private GameObject player;
 
-    private SineCycle ySineCycle;
+    private SineCycle playerSineCycle;
+    private SineCycle enemySineCycle;
 
     public void Start()
     {
-        ySineCycle = new SineCycle(playerSinWaveMagnitude, playerCycleSpeed, player);
+        playerSineCycle = new SineCycle(playerSinWaveMagnitude, playerCycleSpeed, player);
+        enemySineCycle = new SineCycle(enemy, enemySinWaveMagnitude, enemyCycleSpeed);
         LaunchTorpedo();
     }
 
     void Update()
     {
-        MovePlayer();   
+        MovePlayer();
+        MoveEnemy();
+    }
+
+    private void MoveEnemy()
+    {
+        enemySineCycle.Update();
     }
 
     private void MovePlayer()
     {
-        ySineCycle.Update();
+        playerSineCycle.Update();
     }
 
     public void LaunchTorpedo()
