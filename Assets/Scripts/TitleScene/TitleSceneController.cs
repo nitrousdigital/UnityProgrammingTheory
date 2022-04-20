33430b606d88925d6cf438d8268e8f9f3216e9cb
nitrousdigital <nitrousdigital@gmail.com>
2022-04-19 20:25:14 -0400

    exit button

--- Assets/Scripts/TitleScene/TitleSceneController.cs
+++ Assets/Scripts/TitleScene/TitleSceneController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class TitleSceneController : MonoBehaviour
 {
     /// <summary>
@@ -64,5 +68,18 @@ public class TitleSceneController : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    /**
+     * Exit the application (or play mode when in dev mode)
+     */
+    public void Exit()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            // production/runtime code to quit Unity player
+            Application.Quit(); 
+        #endif
     }
 }
